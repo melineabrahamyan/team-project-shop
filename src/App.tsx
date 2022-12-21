8c964ac39bc49e91e3875c81f075fdb8afc8f2cb
Sam <sam.soghomonyan@bk.ru>
2022-12-21 16:20:39 +0400

    homePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import './App.css'
+import "./App.css";
 //pages
 import Home from "./pages/home";
 import About from "./pages/about";
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,3 +1,43 @@
-export default function Footer(){
-    return <div>Footer</div>
+export default function Footer() {
+  return (
+    <div className="footerContaner">
+      <div className="footer">
+        <div className="footerDiv">
+          <h3>Weekday</h3>
+          <p>Stor locator</p>
+          <p>Available jobs</p>
+          <p>Press</p>
+          <p>Responsibility</p>
+          <p>Affiliate</p>
+          <p>Student discount</p>
+        </div>
+        <div className="footerDiv">
+          <h3>Help</h3>
+          <p>Contant us</p>
+          <p>Delivery methods</p>
+          <p>Return & refund policy</p>
+          <p>Size guides</p>
+          <p>Garment care</p>
+          <p>Payment</p>
+        </div>
+        <div className="footerDiv">
+          <h3>Legal</h3>
+          <p>Privacy notice</p>
+          <p>Terms & conditions</p>
+          <p>ADR</p>
+          <p>Cookies settings</p>
+        </div>
+      </div>
+      <div className="footerInfo">
+        <p>
+          Weekday is a Swedish street/fashion brand influenced by youth culture
+          and street style. Founded in 2002, Weekday currently ships to 97
+          markets and has stores in 16 countries, offering a unique retail
+          experience and a curated mix of women’s and men’s assortments as well
+          as a small selection of external brands.
+        </p>
+        <h4>@Weekday</h4>
+      </div>
+    </div>
+  );
 }
