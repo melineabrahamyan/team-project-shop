72ad65435d6d6c0adb263e927d469ba772502597
melineabrahamyan <abrahamyan.meline@gmail.com>
2022-12-26 00:25:33 +0400

    feat(men): added men page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css'
+
 //pages
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -7,12 +8,12 @@ import Cart from "./pages/cart";
 import Category from "./pages/category";
 import Wishlist from "./pages/wishlist";
 import Women from "./pages/women";
-// @ts-ignore
+import Men from "./pages/men";
 import Contact from "./pages/contact";
+
 //components
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-import Gender from "./pages/gender";
 import SingleProduct from "./pages/singleProduct";
 
 
@@ -26,8 +27,8 @@ function App() {
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/:gender" element={<Gender />} /> */}
-        <Route path="/:women" element={<Women />} />
+        <Route path="/womens" element={<Women />} />
+        <Route path="/mens" element={<Men />} />
         <Route path="/:gender/:category" element={<Category />} />
         <Route path="/:gender/:category/:id" element={<SingleProduct />} />
       </Routes>
